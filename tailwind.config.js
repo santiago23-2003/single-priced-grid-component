27bdef08f2ccb449d00644bdea0e7156e8b3a745
santiago23-2003 <santiag636@gmail.com>
2022-06-30 22:37:20 -0500

    Segunda Practica de tailwind en frontendmentor.io

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      "cyan": "#2ab2af",
+      "bright_yellow": "#c0df34",
+      "light_gray": "	#e5eff5",
+      "grayish_blue": "#98a6bd",
+      "white": "#ffffff",      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
